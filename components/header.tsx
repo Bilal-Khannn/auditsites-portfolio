19f37b97d68296abbc37e2e3d5fbad122808ef7c
Bilal Khan <bkhan8423@gmail.com>
2024-05-04 22:53:55 +0500

    logo

--- components/header.tsx
+++ components/header.tsx
@@ -5,15 +5,25 @@ import { FaTelegramPlane } from 'react-icons/fa';
 import { useState } from 'react';
 import Link from 'next/link';
 import { FaXTwitter } from 'react-icons/fa6';
+import Image from 'next/image';
 
 export function Header() {
     const [openDialog, setOpenDialog] = useState<boolean>(false);
     return (
         <div className="flex justify-center sm:justify-between items-center p-6 max-w-7xl mx-auto">
             <Contact openDialog={openDialog} setOpenDialog={setOpenDialog} />
-            <h1 className="font-bold italic text-2xl  lg:text-3xl">
-                Auditorsites
-            </h1>
+            <div className="flex items-center">
+                <Image
+                    src={'/images/logo.svg'}
+                    alt="logo"
+                    width={50}
+                    height={50}
+                />
+                <h1 className="font-bold italic text-2xl  lg:text-3xl">
+                    Auditorsites
+                </h1>
+            </div>
+
             <div className="flex gap-6 items-center">
                 <Link
                     className="text-xl z-50 hidden sm:block text-purple-500 hover:text-purple-400 transition delay-75"
